bc6bc7985f834b844253fb608e65367070f8c3f8
2017eet2680 <push.dahiya@gmail.com>
2018-09-27 14:57:29 +0530

    Commit after completion of ps2

--- ps2.py
+++ ps2.py
@@ -2,39 +2,56 @@
 
 ####### write your code here ##########
 
+######## Fundtion to check character in range of characters####
 def char_range(c1, c2):
     for c in range(ord(c1), ord(c2)+1):
         yield chr(c)
-def rotate(l, n):
-	try:
-		return l[n:] + l[:n]
-	except:
-		return
 
+####### Function to rotate a list of char by y###############
+def rotate(l, y):
+   if len(l) == 0:
+      return l
+   y = -y % len(l) 
+   return l[y:] + l[:y]
+
+####### Main program #########################################
 if __name__ == "__main__":
+
+	# Take the inputs in k1, k2, k3 and s
 	k1=input()
 	k2=input()
 	k3=input()
 	s=input()
+
+	# Initailize 3 empty list for the 3 range of characters
 	l1=[]
 	l2=[]
 	l3=[]
+
+	# Initialize list for output string
 	out=[]
+
+	# Check the character belong to which range
 	for c in s:
 		if c in char_range('a','i'):
-			l1.append(c)
-		if c in char_range('j','r'):
-			l2.append(c)
-		if c in (char_range('s','z') or '_'):
-			l3.append(c)
-	rotate(l1,k1)
-	rotate(l2,k2)
-	rotate(l3,k3)
+			l1+=c
+		elif c in char_range('j','r'):
+			l2+=c
+		elif c in char_range('s','z') or '_':
+			l3+=c
+	# Rotate the elements of list by the value given in k
+	l1=rotate(l1,int(k1))
+	l2=rotate(l2,int(k2))
+	l3=rotate(l3,int(k3))
+
+	# Place the rotated elements in the out list
 	for c in s:
 		if c in char_range('a','i'):
 			out.append(l1.pop(0))
-		if c in char_range('j','r'):
+		elif c in char_range('j','r'):
 			out.append(l2.pop(0))
-		if c in (char_range('s','z') or '_'):
+		elif c in char_range('s','z') or '_':
 			out.append(l3.pop(0))
-	print(out)
+
+	#Print the out list by converting it to a string
+	print(''.join(out))
